22d0cfd61f41f2acdfb7d713f32731a25ecdd0f4
albieta <alba.roma@estudiantat.upc.edu>
2022-12-28 17:52:13 +0100

    little progress

--- app/src/main/java/edu/upc/dsa/mycalculator/Calculator.java
+++ app/src/main/java/edu/upc/dsa/mycalculator/Calculator.java
@@ -2,61 +2,99 @@ package edu.upc.dsa.mycalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class Calculator extends AppCompatActivity {
 
+    TextView input;
     TextView result;
 
-    List<String> operation;
+    Double numericResult;
+
+    LinkedList<String> operation;
+
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
         this.operation = new LinkedList<>();
-        
+
+        this.input = findViewById(R.id.inputText);
         this.result = findViewById(R.id.resultText);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.zeroBtn:
-                break;
-            case R.id.oneBtn:
-                break;
-            case R.id.twoBtn:
-                break;
-            case R.id.threeBtn:
-                break;
-            case R.id.fourBtn:
-                break;
-            case R.id.fiveBtn:
-                break;
-            case R.id.sixBtn:
-                break;
-            case R.id.sevenBtn:
-                break;
-            case R.id.eightBtn:
-                break;
-            case R.id.nineBtn:
-                break;
-            case R.id.sumBtn:
-                break;
-            case R.id.divideBtn:
-                break;
-            case R.id.comaBtn:
-                break;
-            case R.id.equalsBtn:
-                break;
+    @SuppressLint("SetTextI18n")
+    public void addOperationElement(View view) {
+       Button buttonClicked = (Button) view;
+       String text = buttonClicked.getText().toString();
+       operation.add(text);
+       input.setText(input.getText() + text);
+    }
+
+    public void deleteOperationElement(View view) {
+        StringBuffer oldText = new StringBuffer(input.getText().toString());
+        operation.removeLast();
+        input.setText(oldText.deleteCharAt(oldText.length()-1));
+    }
+
+    public void performOperation(View view) {
+        LinkedList<String> operationCorrected = new LinkedList<>();
+        StringBuffer wholeNumber = new StringBuffer();
+
+        for (String value : this.operation){
+            if (isNumber(value)){
+                wholeNumber.append(value);
+                return;
+            }
+            if (!wholeNumber.toString().equals(""))
+                operationCorrected.add(wholeNumber.toString());
+            operationCorrected.add(value);
+            wholeNumber = new StringBuffer();
         }
+        this.operation = operationCorrected;
+        performDivisionMultiplication();
+
+    }
+
+    private void performDivisionMultiplication() {
+        LinkedList<String> operationCorrected = new LinkedList<>();
+        while (true) {
+            int index = firstDivisionOrMultiplication();
+            if(index == -1){
+                return;
+            }
+            if (Objects.equals(this.operation.get(index), "/")){
+                Double result = Double.parseDouble(this.operation.get(index-1))/Double.parseDouble(this.operation.get(index+1));
+                operationCorrected.add(result.toString());
+            }
+
 
         }
     }
+
+    private boolean isNumber(String value){
+        if (!Objects.equals(value, "/") && !Objects.equals(value, "-") &&
+                !Objects.equals(value, "+") && !Objects.equals(value, "x"))
+            return true;
+        return false;
+    }
+
+    private int firstDivisionOrMultiplication() {
+        for(int i = 0; i < this.operation.size(); i++){
+            if (Objects.equals(this.operation.get(i), "/") || Objects.equals(this.operation.get(i), "x")){
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
